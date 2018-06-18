9f10d600d6baa96cea1029ed68abdcb477648ce2
sudhanva <sudhanva.sa@gmail.com>
2018-06-19 00:56:52 +0530

    event handling (app.js)

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-var g =G$('sudhanva','pooja');
-g.greet().setLang('es').greet(false).log();
-console.log(g);
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
+var g = G$('sudhanva', 'pooja');
+
+// use our chainable methods
+g.greet().setLang('es').greet(true).log();
+
+// let's use our object on the click of the login button
+$('#login').click(function() {
+   
+    // create a new 'Greetr' object (let's pretend we know the name from the login)
+    var loginGrtr = G$('sudhanva', 'pooja');
+    
+     // hide the login on the screen
+   // $('#logindiv').hide();
+    
+     // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+    
+});
